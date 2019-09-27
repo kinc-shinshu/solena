b966cfb8cdb83f39d59b49acdfb2205c3ac93b4a
yoidea <yoidea.cc@gmail.com>
2019-09-28 05:24:55 +0900

    :sparkles: Add an id input area, a notify indicator

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,41 @@
 import React from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <div className="hero">
-        <h1>Hello</h1>
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.baseurl = "http://localhost:8080/notify";
+    this.state = {
+      id: "",
+      notify: ""
+    };
+  }
+
+  componentDidMount() {
+    setInterval(this.getNotify, 1000);
+  }
+
+  handleChange = (event) => {
+    this.setState({id: event.target.value});
+  }
+
+  getNotify = async () => {
+    if (!this.state.id) return;
+    const notify = await fetch(`${this.baseurl}/${this.state.id}`)
+      .then(response => response.text());
+    this.setState({ notify });
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <div className="hero">
+          <input className="textbox" type="text" placeholder="id" value={this.state.id} onChange={this.handleChange} />
+          <h1>{this.state.notify}</h1>
+        </div>
       </div>
-    </div>
-  );
+    );
+  }
 }
 
 export default App;
