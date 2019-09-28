f8dc33a5aac19b10a89dda877d3b2624d44ab507
yoidea <yoidea.cc@gmail.com>
2019-09-28 17:57:54 +0900

    Add: Add reaction buttons

--- client/src/App.js
+++ client/src/App.js
@@ -7,12 +7,16 @@ class App extends React.Component {
     this.baseurl = "http://localhost:8080/notify";
     this.state = {
       id: "",
-      messageList: ""
+      messageList: "",
+      messageStack: []
     };
   }
 
   componentDidMount() {
-    setInterval(this.getNotify, 1000);
+    setInterval(this.postNotify, 1000);
+    setTimeout(() => {
+      setInterval(this.getNotify, 1000);    
+    }, 100);
   }
 
   handleChange = (event) => {
@@ -26,12 +30,36 @@ class App extends React.Component {
     this.setState({ messageList: notify.messageList });
   }
 
+  pushMessage = (message) => {
+    this.state.messageStack.push(message);
+  }
+
+  postNotify = async () => {
+    if (!this.state.id) return;
+    const body = this.state.messageStack.map((m) => `body=${m}`).join("&");
+    this.setState({ messageStack: [] });
+    const status = await fetch(`${this.baseurl}/${this.state.id}`, {
+      body,
+      headers: {
+        "Content-Type": "application/x-www-form-urlencoded"
+      },
+      method: "POST"
+    }).then(response => response.status);
+    console.log(status);
+  }
+
   render() {
     return (
       <div className="App">
         <div className="hero">
           <input className="textbox" type="text" placeholder="id" value={this.state.id} onChange={this.handleChange} />
           <h1>{this.state.messageList}</h1>
+          <button className="btn" onClick={
+            () => {this.state.messageStack.push("Good")}
+          }>Good</button>
+          <button className="btn" onClick={
+            () => {this.state.messageStack.push("Bad")}
+          }>Bad</button>
         </div>
       </div>
     );
