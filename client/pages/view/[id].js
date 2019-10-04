d62bf4e0905e5f253915f6587c9c167492fd207e
yoidea <yoidea.cc@gmail.com>
2019-10-04 15:27:04 +0900

    Feat: Create view area

--- client/pages/view/[id].js
+++ client/pages/view/[id].js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Link from 'next/link'
 import { useRouter } from 'next/router'
 import Head from '../../components/head'
@@ -7,12 +7,64 @@ import Nav from '../../components/nav'
 const View = () => {
   const router = useRouter()
   const { id } = router.query
+  const [list, setList] = useState(". . .")
+
+  const getNotify = async (id) => {
+    if (id === undefined) return
+    const baseurl = "http://localhost:8080"
+    const notify = await fetch(`${baseurl}/${id}`)
+      .then(response => response.json());
+    setList(notify.messageList);
+  }
+
+  setInterval(() => { getNotify(id) }, 1000);
   return (
-    <div>
-      <Link href='/view/[id]' as={'/view/111'}>
-        hreflink
-      </Link>
-      <p>ID: {id}</p>
+    <div className="App">
+      <div className="hero">
+        <h1>ID: {id}</h1>
+        <h1>{list}</h1>
+      </div>
+      <style jsx>{`
+        .App {
+            text-align: center;
+          }
+
+          .hero {
+            background-color: #282c34;
+            min-height: 100vh;
+            display: flex;
+            flex-direction: column;
+            align-items: center;
+            justify-content: center;
+            font-size: calc(10px + 2vmin);
+            color: white;
+          }
+
+          .textbox {
+            font-size: 30px;
+            background-color: #282c34;
+            border-color: white;
+            border-style: solid;
+            border-width: 1px;
+            border-radius: 5px;
+            height: 40px;
+            padding: 5px;
+            color: white;
+          }
+
+          .btn {
+            font-size: 30px;
+            background-color: #282c34;
+            border-color: white;
+            border-style: solid;
+            border-width: 1px;
+            border-radius: 5px;
+            height: 50px;
+            padding: 10px;
+            margin: 5px;
+            color: white;
+          }
+      `}</style>
     </div>
   )
 }
